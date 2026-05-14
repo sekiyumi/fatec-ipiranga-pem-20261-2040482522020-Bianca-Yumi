1158ace5c6bfaecb06945c3cd2d86dab754c841c
Yumi Seki <yumisekii@gmail.com>
2026-05-14 08:55:41 -0300

    Create interface.h

---
+++ PEM-20261-Semana9/interface.h
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------------------*/
+/*  FATEC-Ipiranga      ADS Verspertino                                     */
+/*  Bianca Yumi Seki    -    204048252202                                   */
+/*  Objetivo: Desenvolver um sistema que execute cáuculos complexos         */
+/*            utilizando função recursiva                                   */
+/*                                                        Data: 08/05/2026  */
+/*--------------------------------------------------------------------------*/
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+void exibirMenu();
+int lerDado(char mensagem[]);
+#endif
