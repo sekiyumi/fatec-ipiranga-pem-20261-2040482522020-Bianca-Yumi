0e6dba3e0111309623465109f5423144ed8aecf4
Yumi Seki <yumisekii@gmail.com>
2026-05-14 08:56:35 -0300

    Create recursao.h

---
+++ PEM-20261-Semana9/recursao.h
@@ -0,0 +1,14 @@
+/*--------------------------------------------------------------------------*/
+/*  FATEC-Ipiranga      ADS Verspertino                                     */
+/*  Bianca Yumi Seki    -    204048252202                                   */
+/*  Objetivo: Desenvolver um sistema que execute cáuculos complexos         */
+/*            utilizando função recursiva                                   */
+/*                                                        Data: 08/05/2026  */
+/*--------------------------------------------------------------------------*/
+#ifndef RECURSAO_H
+#define RECURSAO_H
+
+void decToBin(int n);
+int potencia(int base,int exp);
+int somaDigitos(int n);
+#endif
