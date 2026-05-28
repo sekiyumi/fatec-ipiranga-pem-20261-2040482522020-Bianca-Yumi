39d065d301fe69c05a57546018bf3163bba5b47c
Yumi Seki <yumisekii@gmail.com>
2026-05-28 19:21:35 -0300

    Create stringutil.h

---
+++ PEM-20261-Semana14/stringutil.h
@@ -0,0 +1,14 @@
+/*--------------------------------------------------------------------------*/
+/*  FATEC-Ipiranga      ADS Verspertino                                     */
+/*  Bianca Yumi Seki    -    204048252202                                   */
+/*  Objetivo:   Biblioteca de operações com string                          */
+/*                                                        Data: 22/05/2026  */
+/*--------------------------------------------------------------------------*/
+#ifndef stringutil_h
+#define stringutil_h
+
+int contaVogais(char *s);
+void inverteCString(char *s);
+int ePalindromo(char *s);
+
+#endif
